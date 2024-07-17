1ced8f22039cfef55235d26db0be1b3399a33c5d
Michał Górny <mgorny@gentoo.org>
2024-07-17 05:12:00 +0200

    Unpin systemd
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- build.bash
+++ build.bash
@@ -125,7 +125,7 @@ export_vars() {
 						sys-apps/rng-tools
 						sys-apps/sed
 						sys-apps/shadow
-						<sys-apps/systemd-256
+						sys-apps/systemd
 						sys-apps/util-linux
 						sys-auth/polkit
 						sys-block/nbd
